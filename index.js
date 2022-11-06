df6f290618eed5dded4dc5aa72247e3cc564e062
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-06 15:42:53 +0600

    delete order

--- index.js
+++ index.js
@@ -59,6 +59,14 @@ async function run() {
             res.send(orders);
         });
 
+        //delete order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
